f19a32e8a7af2de607824c847d09f10b6d269282
Phuripat Chanaphai <65070181@kmitl.ac.th>
2024-10-25 08:21:33 +0000

    1_commit

--- netmiko_final.py
+++ netmiko_final.py
@@ -1,12 +1,12 @@
 from netmiko import ConnectHandler
 from pprint import pprint
 
-device_ip = "<!!!REPLACEME with router IP address!!!>"
+device_ip = "10.0.15.183"
 username = "admin"
 password = "cisco"
 
 device_params = {
-    "device_type": "<!!!REPLACEME with device type for netmiko!!!>",
+    "device_type": "cisco_ios",
     "ip": device_ip,
     "username": username,
     "password": password,
@@ -19,16 +19,24 @@ def gigabit_status():
         up = 0
         down = 0
         admin_down = 0
-        result = ssh.send_command("<!!!REPLACEME with proper command!!!>", use_textfsm=True)
+        result = ssh.send_command("sh ip int br", use_textfsm=True)
         for status in result:
-            if <!!!Write code here!!!>:
-                <!!!Write code here!!!>
-                if <!!!Write code here!!!> == "up":
+            pprint(status)
+            print("Keys in status dictionary:", status.keys())
+
+            # Adjust key names based on observed output
+            interface = status.get("interface", "")
+            status_value = status.get("status", "")
+            if "GigabitEthernet" in status:
+                if status["status"] == "up":
                     up += 1
-                elif <!!!Write code here!!!> == "down":
+                elif status["status"] == "down":
                     down += 1
-                elif <!!!Write code here!!!> == "administratively down":
+                elif status["status"] == "administratively down":
                     admin_down += 1
-        ans = <!!!Write code here!!!>
+                ans += f"{interface} is {status_value}, "
+
+        #output GigabitEthernet1 up, GigabitEthernet2 up, GigabitEthernet3 down, GigabitEthernet4 administratively down -> 2 up, 1 down, 1 administratively down
+        ans += f"-> {up} up, {down} down, {admin_down} administratively down"
         pprint(ans)
         return ans
